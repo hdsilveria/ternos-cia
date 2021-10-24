c7157aacc644f985b02b6f33ad0fb53953cb03e5
Henrique <rique.duarte@outlook.com.br>
2021-10-24 17:17:40 -0300

    Created effects in the menu;

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
 <div id="app">
 
-<div class="w3-bar w3-top w3-border" id="menu">
-  <div class="w3-right">
-      <a class="w3-bar-item w3-button w3-hover-white w3-hover-text-teal" @click="empresa">Empresa</a>
-      <a class="w3-bar-item w3-button w3-hover-white w3-hover-text-teal" @click="camisaria">Camisaria</a>
-      <a class="w3-bar-item w3-button w3-hover-white w3-hover-text-teal" @click="alfaiateria">Alfaiateria</a>
-      <a class="w3-bar-item w3-button w3-hover-white w3-hover-text-teal" @click="depoimentos">Depoimentos</a>
-      <a class="w3-bar-item w3-button w3-hover-white w3-hover-text-teal" @click="contato">Contato</a>
-    </div>
-  </div>
+<b-row id="menu" class="w-100" align-h="end">
+  <b-col cols="auto" align-self="center">
+    <ul class="mt-1 d-flex">
+      <li>
+        <a @click="empresa">Empresa</a>
+      </li>
 
-<empresa class="empresa" />
-<sobre class="sobre" />
-<opiniao class="opiniao" />
-<qualidades class="qualidades" />
-<condicoes class="condicoes" />
-<camisaria  class="camisaria"/>
-<depoimentos  class="depoimentos"/>
-<contatos  class="contatos"/>
+      <li>
+        <a @click="camisaria">Camisaria</a>
+      </li>
+
+      <li>
+        <a @click="alfaiateria">Alfaiateria</a>
+      </li>
+
+      <li>
+        <a @click="depoimentos">Depoimentos</a>
+      </li>
+
+      <li>
+        <a @click="contato">Contato</a>
+      </li>
+    </ul>
+  </b-col>
+</b-row>
+
+<div class="content-page">
+  <empresa class="empresa" />
+  <sobre class="sobre" />
+  <opiniao class="opiniao" />
+  <qualidades class="qualidades" />
+  <condicoes class="condicoes" />
+  <camisaria  class="camisaria"/>
+  <depoimentos  class="depoimentos"/>
+  <contatos  class="contatos"/>
+</div>
 
 
 <footer>
@@ -30,6 +48,11 @@
 </template>
 
 <script>
+
+window.addEventListener("scroll", () => {
+  let header = document.getElementById("menu")
+  header.classList.toggle("sticky", window.scrollY > 150)
+})
 
 import empresa from './components/empresa'
 import sobre from './components/sobre'
@@ -54,37 +77,36 @@ export default {
     camisaria
   },
 
-  methods:{
-
-    empresa: function () {
+  methods: {
+    empresa(){
       scrollToElement('.sobre', {
         offset: 0,
         duration: 2500
     })
   },
 
-    camisaria: function () {
+    camisaria(){
       scrollToElement('.camisaria', {
         offset: 0,
         duration: 2500
     })
   },
 
-    alfaiateria: function () {
+    alfaiateria(){
       scrollToElement('.camisaria', {
         offset: 0,
         duration: 2500
     })
   },
 
-    depoimentos: function () {
+    depoimentos(){
       scrollToElement('.depoimentos', {
         offset: 0,
         duration: 2500
     })
   },
 
-    contato: function () {
+    contato(){
       scrollToElement('.contatos', {
         offset: 0,
         duration: 2500
@@ -102,15 +124,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 #menu {
   padding: 1%;
-  background-color: rgba(255, 255, 255, 0.979);
-  color: #8a8a8a;
+  color: #ffffff;
   font-size: 14pt;
+  position: fixed;
+  transition: 1s;
+  top: 0;
+  left: 0;
+  width: 101% !important;
+  z-index: 99;
 }
+
+.sticky {
+  background-color: rgb(255, 255, 255) !important;
+  color: #1d1d1d9f !important;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.199);
+}
+
+#menu ul li {
+  list-style: none;
+}
+
+#menu ul li a {
+  margin: 0 15px;
+  cursor: pointer;
+}
+
 
 @media screen and (max-width: 650px) {
 #app {
